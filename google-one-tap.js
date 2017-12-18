70b67e0f382d6a89e244b10f3ded7aaf91599206
Matjaž <matjaz.zavski@agiledrop.com>
2017-12-18 16:21:28 +0100

    first commit

---
+++ google-one-tap.js
@@ -0,0 +1,60 @@
+/**
+ * @file
+ * Google One Tap behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.googleOneTap = {
+    attach: function (context, settings) {
+          // The 'googleyolo' object is ready for use.
+
+        window.onGoogleYoloLoad = (googleyolo) => {
+        if (settings['logged_out'] !== undefined) {
+          googleyolo.disableAutoSignIn();
+        }
+        else {
+          // The 'googleyolo' object is ready for use.
+          googleyolo
+          .retrieve({
+            supportedAuthMethods: [
+              "https://accounts.google.com",
+              "googleyolo://id-and-password"
+            ],
+            supportedIdTokenProviders: [
+              {
+                uri: "https://accounts.google.com",
+                clientId: settings['client_id']
+              }
+            ]
+          })
+          .then(
+            (credential) => {
+            $.ajax({
+            url: '?q=google-one-tap/login',
+            type: 'POST',
+            data: credential,
+            dataType: 'json',
+            success: function (xssFilteredValue) {
+              window.location.reload();
+            },
+            error: function (xssFilteredValue) {
+              alert(Drupal.t('One Tap Sign-In Failed'));
+              console.log(xssFilteredValue);
+            }
+          });
+        },
+          (error) => {
+            console.log(error.type);
+            alert(error);
+          });
+        };
+      }
+    }
+  };
+} (jQuery, Drupal));
